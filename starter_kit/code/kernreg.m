ae42425dd719c9098969f20743d7d78d2573cac8
Tyler Brown <brownty@sas.upenn.edu>
2012-11-27 02:38:52 -0500

    Added kernel regression.

---
+++ starter_kit/code/kernreg.m
@@ -0,0 +1,22 @@
+function [ yhat ] = kernreg(Xt_raw, Xq_raw, Yt, sigma)
+% kernreg Summary of this function goes here
+%   Detailed explanation goes here
+Xt = bsxfun(@rdivide, Xt_raw, sum(Xt_raw, 2));
+Xq = bsxfun(@rdivide, Xq_raw, sum(Xq_raw, 2));
+
+D = Xq*Xt'; %Note I think D is actually D^2?
+
+kernMat = exp(-D/(sigma.^2));
+
+votes = zeros(size(Xq_raw,1),10);
+
+for i = 1:10
+    votes(i) = sum(kernMat(Yt==i, :));
+end
+
+% By this point votes is an M x 10 matrix such that votes(i,j) should be
+% the weighted vote that example i is labeled j
+
+[null, yhat] = sort(votes, 2, 'descend');
+
+end
